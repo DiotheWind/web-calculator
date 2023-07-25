147d17255aca6acacbfc98827e0d76669d19ee74
Deo Seth <diothewind@gmail.com>
2023-07-25 20:52:51 +0800

    Add operate function and variables to hold two numbers and an operator

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let leftOperand;
+let rightOperand;
+let operator;
+
+function operate(num1, num2, operator) {
+    let ans;
+    switch(operator) {
+        case "+":
+            ans = add(num1, num2);
+            break;
+        case "-":
+            ans = subtract(num1, num2);
+            break;
+        case "*":
+            ans = multiply(num1, num2);
+            break;
+        case "/":
+            ans = divide(num1, num2);
+            break;
+    }
+
+    return ans;
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
